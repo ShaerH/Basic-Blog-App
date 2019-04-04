38436da110049bbab9ca31f874540229d9238706
Shaer Hasan <shaer2m@gmail.com>
2019-04-04 17:07:26 +0000

    fixed update and added a delete route

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express         = require("express"),
+methodOverride      = require("method-override"),
 mongoose            = require("mongoose"),
 bodyParser          = require("body-parser"),
 app                 = express();
 
 mongoose.connect('mongodb://localhost:27017/blog_app', { useNewUrlParser: true }); 
+mongoose.set('useFindAndModify', false);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 //Basic setup above ============================================================
 
 //Mongoose/Model Config
@@ -21,8 +24,19 @@ var Blog = mongoose.model("Blogs", blogSchema);
 // Blog.create({
 //   title: "Test Blog",
 //   image: "https://images.unsplash.com/photo-1508780709619-79562169bc64?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+//   body: "This is a blog post if its not obvious This is a blog post if its not obvious This is a blog post if its not obvious This is a blog post if its not obvious"
+// });
+// Blog.create({
+//   title: "Test Blog",
+//   image: "https://images.unsplash.com/photo-1554185256-7b994c659b88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60",
+//   body: "This is a blog post if its not obvious This is a blog post if its not obvious This is a blog post if its not obvious"
+// });
+// Blog.create({
+//   title: "Test Blog",
+//   image: "https://images.unsplash.com/photo-1554224212-3d13a4e17935?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60",
 //   body: "This is a blog post if its not obvious"
 // });
+
 
 //RESTful Routes
 app.get('/', function(req, res) {
@@ -81,14 +95,25 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 //Update route
 app.put("/blogs/:id", function(req, res){
-    var id = req.params.id;
-    Blog.findByIdAndUpdate(id,req.body.blog,function(err, updatedBlog){
+    
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog,function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
         } else {
-          res.redirect("/blogs/"+id);  
+          res.redirect("/blogs/" + req.params.id);  
         }
-    })
+    });
+});
+
+//Delete route
+app.delete("/blogs/:id",function(req,res){
+     Blog.findByIdAndDelete(req.params.id, function(err){
+         if(err){
+             res.redirect("/blogs");
+         } else {
+             res.redirect("/blogs");
+         }
+     });
 });
 
 //Basic setup below ============================================================
